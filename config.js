d8d03ebc394148645772839018f8866d1c610e48
DominicDev <s0l0estudi4r@gmail.com>
2025-10-03 15:47:39 +0200

    v0.0.2

---
+++ config.js
@@ -0,0 +1,23 @@
+export const nameBand = 'HOODIE FORESTER';
+export const nameSongs = [
+	"WHISTLE", /*1*/
+	"TRIPPED UP", /*2*/
+	"LINK", /*3*/
+	"GYM", /*4*/
+	"WHISTLE (FEMALE VERSION)", /*5*/
+	"TBT", /*6*/
+	"FUNK AGRESSIVA", /*7*/
+	"REVOLUTION", /*8*/
+	"AURA", /*9*/
+	"CYBER", /*10*/
+	"TIMOCHE FUNK", /*11*/
+	"X-FUNK", /*12*/
+	"TOKYO!", /*13*/
+	"TOKYO! (SPED UP)", /*14*/
+	"X-SPACE", /*15*/
+	"COSMIC", /*16*/
+	"SAKIO", /*17*/
+	"TUKA TUKA", /*18*/
+	"TREMOR", /*19*/
+	"ERA", /*20*/
+]
